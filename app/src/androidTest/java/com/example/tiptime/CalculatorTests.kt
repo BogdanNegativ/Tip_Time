ff128ce8ff6c1a5c4c8894ac07adddc0482ae03c
Bohdan Liashuk <luashuk2002@gmail.com>
2023-11-10 21:33:29 +0200

    Add Rule

--- app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
+++ app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
@@ -1,9 +1,12 @@
 package com.example.tiptime
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class CalculatorTests {
-
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
 }
