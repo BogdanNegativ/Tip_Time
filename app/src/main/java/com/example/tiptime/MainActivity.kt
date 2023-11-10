70b30e5be9266a8aa9132af89fac6e81f98cae5f
Bohdan Liashuk <luashuk2002@gmail.com>
2023-11-10 14:43:39 +0200

    Get tip percentage in fun

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -15,5 +15,11 @@ class MainActivity : AppCompatActivity() {
     fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
     }
 }
